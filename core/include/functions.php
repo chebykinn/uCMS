da10c78966ef462d27823bdb42f7d2289feae34e
IVaN4B <ivan4b69@gmail.com>
2015-04-11 16:19:48 +0300

    Moved log_add function to debug.php

--- core/include/functions.php
+++ core/include/functions.php
@@ -28,43 +28,4 @@ function get_current_action(){
 	$url = new URLManager();
 	return $url->getCurrentAction();
 }
-
-
-function log_add($message, $level = UC_LOG_INFO){
-	if(LOG_LEVEL > $level){
-		switch ($level) {
-			case UC_LOG_INFO:
-				$type = '[INFO]';
-			break;
-
-			case UC_LOG_NOTICE:
-				$type = '[NOTICE]';
-			break;
-
-			case UC_LOG_WARNING:
-				$type = '[WARNING]';
-			break;
-
-			case UC_LOG_ERROR:
-				$type = '[ERROR]';
-			break;
-
-			case UC_LOG_CRITICAL:
-				$type = '[CRITICAL]';
-			break;
-			
-			default:
-				$type = '[INFO]';
-			break;
-		}
-		$outMessage = strftime("%Y-%m-%d %H:%M", time())." $type $message\n";
-		$logFile = fopen(LOG_FILE, 'a');
-		fwrite($logFile, $outMessage);
-		fclose($logFile);
-		if($level === UC_LOG_CRITICAL){
-			p($outMessage);
-			die;
-		}
-	}
-}
 ?>
