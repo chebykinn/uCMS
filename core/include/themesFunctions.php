226868300845129514aaa9dd9e28eb7592bf922c
Ivan <ivan4b69@gmail.com>
2015-04-22 00:37:50 +0300

    Changed interaction with static and singleton classes

--- core/include/themesFunctions.php
+++ core/include/themesFunctions.php
@@ -5,35 +5,35 @@ function get_load_time(){
 }
 
 function get_queries_count(){
-	return uCMS::getInstance()->getDatabase()->getQueriesCount();
+	return DatabaseConnection::getDefault()->getQueriesCount();
 }
 
 function get_header(){
-	return uCMS::getInstance()->getCurrentTheme()->loadBlock('header');
+	return Theme::getCurrent()->loadBlock('header');
 }
 
 function get_sidebar(){
-	return uCMS::getInstance()->getCurrentTheme()->loadBlock('sidebar');
+	return Theme::getCurrent()->loadBlock('sidebar');
 }
 
 function get_footer(){
-	return uCMS::getInstance()->getCurrentTheme()->loadBlock('footer');
+	return Theme::getCurrent()->loadBlock('footer');
 }
 
 function get_style(){
-	return uCMS::getInstance()->getCurrentTheme()->getURLFilePath(uCMS::getInstance()->getCurrentTheme()->getInfo('style'));
+	return Theme::getCurrent()->getURLFilePath(Theme::getCurrent()->getInfo('style'));
 }
 
 function get_title(){
-	return uCMS::getInstance()->getCurrentTheme()->getTitle();
+	return Theme::getCurrent()->getTitle();
 }
 
 function error_404(){
-	$theme = get_setting('theme');
+	$theme = Settings::get('theme');
 	if(empty($theme)) $theme = DEFAULT_THEME;
 	if( file_exists(get_theme_path($theme).ERROR_TEMPLATE) ){
-		if( !is_object( uCMS::getInstance()->getCurrentTheme() ) || $theme != uCMS::getInstance()->getCurrentTheme()->getName() ) uCMS::getInstance()->reloadTheme($theme);
-		uCMS::getInstance()->getCurrentTheme()->setTitle(tr("404 Not Found"));
+		if( $theme != Theme::getCurrent()->getName() ) uCMS::getInstance()->reloadTheme($theme);
+		Theme::getCurrent()->setTitle(tr("404 Not Found"));
 		include get_theme_path($theme).ERROR_TEMPLATE;
 		exit;
 	}
