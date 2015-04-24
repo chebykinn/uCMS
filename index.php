893757e85675f3dce7d40fb1185a4d4dc2b710b2
Ivan <ivan4b69@gmail.com>
2015-04-25 00:34:49 +0300

    Changed ABSPATH definition

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 if( file_exists('config.php') ){
 	require 'config.php';
 }else if( file_exists('../config.php') ){
-	define("ABSPATH", dirname(__FILE__)."/");
+	define("ABSPATH", getcwd()."/");
 	require '../config.php';
 }
 
 if( !defined("ABSPATH") ){
 	//install
+	echo "install";
+	exit;
 }
 
 uCMS::getInstance()->init();
 
 uCMS::getInstance()->runSite();
+exit;
 ?>
