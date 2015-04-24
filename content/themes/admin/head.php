9b7901b81255c43874fe555ebc64347fcdc87166
Ivan <ivan4b69@gmail.com>
2015-04-25 00:28:57 +0300

    Added support for mobile phones

--- content/themes/admin/head.php
+++ content/themes/admin/head.php
@@ -3,9 +3,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_style(); ?>">
+	<link rel="stylesheet" href="<?php echo get_style('mobile'); ?>" type="text/css" media="handheld,only screen and (max-device-width:480px)">
 	<title><?php echo get_title(); ?></title>
 </head>
 <body>
 <div id="wrapper">
+<a href="#sidebar" class="show-sidebar"></a>
