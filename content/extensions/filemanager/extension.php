237911c1c7c02358dcbbb8c3fe25804ae5e7c29e
Ivan <ivan4b69@gmail.com>
2015-04-25 00:24:03 +0300

    Created a file manager extension skeleton

---
+++ content/extensions/filemanager/extension.php
@@ -0,0 +1,20 @@
+<?php
+class FileManager extends Extension{
+	
+	public function install(){
+
+	}
+
+	public function uninstall(){
+
+	}
+
+	public function load(){
+		
+	}
+
+	public function doAdminAction($action){
+		return tr('File Manager');
+	}
+}
+?>
---
+++ content/extensions/filemanager/file.php
@@ -0,0 +1,16 @@
+<?php
+class File{
+	private $fid;
+	private $uid;
+	private $name;
+	private $type;
+	private $status;
+	private $location;
+	private $size;
+	private $created;
+
+	public function __construct(){
+		
+	}
+}
+?>
