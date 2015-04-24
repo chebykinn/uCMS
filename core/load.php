0533cb26ef0f9dd4b4976e93f07d507d0df44357
Ivan <ivan4b69@gmail.com>
2015-04-25 00:33:26 +0300

    Renamed includes.php to load.php

---
+++ core/load.php
@@ -0,0 +1,19 @@
+<?php
+require_once ABSPATH.'core/constants.php';
+/**
+* @todo move files to more logical locations
+*/
+require_once CORE_PATH.'procedural/debug.php';
+require_once CORE_PATH.'procedural/general.php';
+require_once CORE_PATH.'language.php';
+require_once CORE_PATH.'database/databaseConnection.php';
+require_once CORE_PATH.'database/query.php';
+require_once CORE_PATH.'session.php';
+require_once CORE_PATH.'extension.php';
+require_once CORE_PATH.'theme.php';
+require_once CORE_PATH.'procedural/themes.php';
+require_once CORE_PATH.'extensions.php';
+require_once CORE_PATH.'urlManager.php';
+require_once CORE_PATH.'settings.php';
+require_once CORE_PATH.'ucms.php';
+?>
