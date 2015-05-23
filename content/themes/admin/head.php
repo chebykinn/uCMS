34d58709fabc46dae30396403781241b6d61e838
Ivan <ivan4b69@gmail.com>
2015-05-23 22:36:05 +0300

    Added mobile support

--- content/themes/admin/head.php
+++ content/themes/admin/head.php
@@ -6,7 +6,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_style(); ?>">
-	<link rel="stylesheet" href="<?php echo get_style('mobile'); ?>" type="text/css" media="handheld,only screen and (max-device-width:480px)">
+	<link rel="stylesheet" href="<?php echo get_style('mobile'); ?>" type="text/css" media="handheld,only screen and (max-device-width:960px)">
+	<script type="text/javascript" src="<?php echo get_theme_file('jquery'); ?>"></script>
+	<script type="text/javascript" src="<?php echo get_theme_file('mobile-js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo get_theme_file('table-js'); ?>"></script>
 	<title><?php echo get_title(); ?></title>
 </head>
 <body>
