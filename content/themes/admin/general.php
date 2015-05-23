92254f065cca6d0ac2db413f3a7f57d35a4efe1b
Ivan <ivan4b69@gmail.com>
2015-05-23 22:35:48 +0300

    Updated admin theme behavior

--- content/themes/admin/general.php
+++ content/themes/admin/general.php
@@ -1,73 +1,80 @@
 <?php
 // Admin theme control center
-require_once ADMIN_PATH.'adminPanel.php';
+require_once ADMIN_PATH.'controlPanel.php';
 require_once ADMIN_PATH.'manageTable.php';
 require_once ADMIN_PATH.'managePage.php';
 
-AdminPanel::init();
-$defaultActions = array('settings', 'extensions', 'phpinfo');
-$defaultTitles = array('settings' => tr('Settings'), 'extensions' => tr('Extensions'), 'phpinfo' => tr('PHP Information'), '' => tr('Home'));
-$currentAction = AdminPanel::getAction();
-$adminActions = array_merge($defaultActions, Extensions::getUsedAdminActions());
+ControlPanel::Init();
+$defaultTitles = array(
+	''           => tr('Home'),
+	'home'       => tr('Home'),
+	'settings'   => tr("Settings"), 
+	'extensions' => tr("Extensions"), 
+	'themes'     => tr("Themes"), 
+	'widgets'    => tr("Widgets"), 
+	'tools'      => tr("Tools"),
+	'phpinfo'    => tr("PHP Information"),
+	'journal'    => tr("System Journal")
+);
+$defaultActions = array_keys($defaultTitles);
+$currentAction = ControlPanel::GetAction();
+$adminActions = array_merge($defaultActions, Extensions::GetUsedAdminActions());
 if( !empty($currentAction) && !in_array($currentAction, $adminActions) ){
 	error_404();
 }
-if( User::current()->can('access control panel') ){
+if( User::Current()->can('access control panel') ){
 	$adminTitle = $this->getTitle().' :: ';
 	if( isset($defaultTitles[$currentAction]) ) $this->setTitle($adminTitle.$defaultTitles[$currentAction]);
 	get_header();
 	get_sidebar();
 	switch ( $currentAction ) {
 		case '':
+		case 'home':
 			$this->loadTemplate('index');
 		break;
 	
 		case 'settings':
-			$extensionAction = URLManager::getKeyValue('settings');
+			$extensionAction = URLManager::GetKeyValue('settings');
 			if( !empty($extensionAction) ){
 				$settingsAction = 'settings/'.$extensionAction;
-				$extension = Extensions::getExtensionByAdminAction($settingsAction);
+				$extension = Extensions::GetExtensionByAdminAction($settingsAction);
 				if( is_object($extension) ){
 					$pageFile = $extension->getAdminPageFile($settingsAction);
 				}
 				if( !empty($extension) && !empty($pageFile) ){
 					include $pageFile;
 				}else{
-					log_add(tr("Unable to load admin page for action: @s", $currentAction), UC_LOG_ERROR);
-					URLManager::redirect(URLManager::makeLink('admin', 'settings', true));
+					log_add(tr("Unable to load admin page for action: @s", $settingsAction), UC_LOG_ERROR);
+					URLManager::Redirect(URLManager::MakeLink('admin', 'settings', true));
 				}
 			}else{
 				$this->loadTemplate('settings');
 			}
 		break;
-	
-		case 'extensions':
-			$this->loadTemplate('extensions');
-		break;
-
-		case 'phpinfo':
-			$this->loadTemplate('phpinfo');
-		break;
 		
 		default:
-			$extension = Extensions::getExtensionByAdminAction($currentAction);
-			if( is_object($extension) ){
-				$pageFile = $extension->getAdminPageFile($currentAction);
-			}
-			if( !empty($extension) && !empty($pageFile) ){
-				include $pageFile;
+			if( in_array($currentAction, $defaultActions) ){
+				$this->loadTemplate($currentAction);
 			}else{
-				log_add(tr("Unable to load admin page for action: @s", $currentAction), UC_LOG_ERROR);
-				URLManager::redirect(URLManager::makeLink('admin'));
+				$extension = Extensions::getExtensionByAdminAction($currentAction);
+				if( is_object($extension) ){
+					$pageFile = $extension->getAdminPageFile($currentAction);
+				}
+				if( !empty($extension) && !empty($pageFile) ){
+					include $pageFile;
+				}else{
+					log_add(tr("Unable to load admin page for action: @s", $currentAction), UC_LOG_ERROR);
+					URLManager::Redirect(URLManager::MakeLink('admin'));
+				}
 			}
 		break;
 	}
 	get_footer();
 }else{
-	if( !User::current()->isLoggedIn() ){
+	if( !User::Current()->isLoggedIn() ){
 		$this->loadTemplate('login');
 	}else{
-		$this->loadTemplate('accessdenied');
+		$this->loadTemplate('access_denied');
 	}
 }
 ?>
