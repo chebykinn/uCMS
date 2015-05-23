1e7e0f8097b1ee891183fa2f10ef38061ef218d8
Ivan <ivan4b69@gmail.com>
2015-05-23 22:34:23 +0300

    Started Permissions class

---
+++ content/extensions/users/include/permission.php
@@ -0,0 +1,21 @@
+<?php
+class Permission{
+	private $list;
+
+	public function __construct(){
+
+	}
+
+	public function add($name, $owner){
+
+	}
+
+	public function rename($oldName, $newName){
+
+	}
+
+	public function remove($name){
+
+	}
+}
+?>
