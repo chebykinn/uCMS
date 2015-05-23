f5a2f2288293657dda2da00367228fb648f44e6c
Ivan <ivan4b69@gmail.com>
2015-05-23 22:41:51 +0300

    Changed debug methods appearance

--- core/procedural/debug.php
+++ core/procedural/debug.php
@@ -1,8 +1,23 @@
 <?php
-function varDump($var){
-	echo '<pre style="text-align: left; color: #000; background: #fff; border: 1px #555 solid; margin: 20px; padding: 5px; z-index: 9999;">';
-	var_dump($var);
+function begin_debug_block(){
+	echo '<pre style="
+	text-align: left; 
+	background: #fff; 
+	color: #000; 
+	padding: 5px;
+	border: 1px #aa1111 solid; 
+	margin: 20px; 
+	z-index: 9999;">';
+}
+
+function end_debug_block(){
 	echo '</pre>';
+}
+
+function varDump($var){
+	begin_debug_block();
+	var_dump($var);
+	end_debug_block();
 }
 
 function log_add($message, $level = UC_LOG_INFO){
@@ -32,7 +47,8 @@ function log_add($message, $level = UC_LOG_INFO){
 				$type = '[INFO]';
 			break;
 		}
-		$outMessage = strftime("%Y-%m-%d %H:%M", time())." $type $message\n";
+		$host = Session::getCurrent()->getHost();
+		$outMessage = strftime("%Y-%m-%d %H:%M:%S", time())." [Host: $host] $type $message\n";
 		$logFile = fopen(LOG_FILE, 'a');
 		if($logFile){
 			fwrite($logFile, $outMessage);
