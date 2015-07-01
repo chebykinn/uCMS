741d3dc6d49cf226b77ebab694f9fad2384c897c
IVaN4B <ivan4b69@gmail.com>
2015-07-01 23:26:16 +0500

    Changed notification system

--- core/admin/managePage.php
+++ core/admin/managePage.php
@@ -2,65 +2,29 @@
 class ManagePage{
 	private $adminAction;
 	private $actions = array();
-	private $notifications = array();
-	private $notificationTypes = array();
 
 	public function __construct(){
-		$this->adminAction = URLManager::getCurrentAdminAction();
-		$this->notificationTypes = array('success', 'error', 'warning');
+		$this->adminAction = ControlPanel::GetAction();
 	}
 
-	public function addNotification($name, $message, $type = ''){
-		if( !in_array($type, $this->notificationTypes) ) {
-			$type = $this->notificationTypes[SUCCESS_STATUS];
-		}
-		$this->notifications[$name][$type] = array('message' => $message);
-	}
-
-	public function showNotifications(){
-		$name = URLManager::getKeyValue('alert');
-		$exitLink = URLManager::makeLink(ADMIN_ACTION, $this->adminAction);
-
-		if( !empty($this->notifications[$name]) ){
-			$alert = URLManager::getKeyValue($name);
-			if( !in_array($alert, $this->notificationTypes) ) {
-				URLManager::redirect($exitLink);
-				return false;
-			}
-			if( !isset($this->notifications[$name][$alert]) ) {
-				URLManager::redirect($exitLink);
-				return false;
-			}
-			echo '<div class="'.$alert.'">'.$this->notifications[$name][$alert]['message'].'</div>';
-			return true;
-		}
-		if( !empty($name) ){
-			URLManager::redirect($exitLink);
-		}
-	}
 
 	public function addAction($name, $permission, $callback, $args = array()){
 		$this->actions[$name] = array('callback' => $callback, 'args' => $args, 'permission' => $permission);
 	}
 
 	public function doActions(){
-		$name = URLManager::getKeyValue($this->adminAction);
-		$exitLink = URLManager::makeLink(ADMIN_ACTION, $this->adminAction);
+		$name = Page::GetCurrent()->getKeyValue($this->adminAction);
+		$exitPage = Page::FromAction(ADMIN_ACTION, $this->adminAction);
 		if( !empty($this->actions[$name]) ){
-			$key = URLManager::getKeyValue($name);
+			$key = htmlspecialchars(Page::GetCurrent()->getKeyValue($name));
 			$user = User::current();
 			if( empty($key) || !$user->can($this->actions[$name]['permission']) || !is_callable($this->actions[$name]['callback']) ){
-				URLManager::redirect($exitLink);
+				$exitPage->go();
 				return false;
 			}
-			$status = (int)call_user_func_array($this->actions[$name]['callback'], array_merge(array($key), $this->actions[$name]['args']));
-			$alert = ( isset($this->notificationTypes[$status]) ) ? $this->notificationTypes[$status] : $this->notificationTypes[SUCCESS_STATUS];
-			$alertLink = URLManager::makeLink(ADMIN_ACTION, $this->adminAction."/alert/$name/$alert");
-			URLManager::redirect($alertLink);
-		}
-		$alertValue = URLManager::getKeyValue('alert');
-		if( !empty($name) && ($name != 'alert' || empty($alertValue)) ){
-			URLManager::redirect($exitLink);
+			$notification = (int)call_user_func_array($this->actions[$name]['callback'], array_merge(array($key), $this->actions[$name]['args']));
+			
+			$exitPage->go();
 		}
 	}
 
---
+++ core/notification.php
@@ -0,0 +1,66 @@
+<?php
+class Notification{
+	private $type;
+	private $message;
+	private $page;
+	private $amount;
+	const SUCCESS = 'success';
+	const WARNING = 'warning';
+	const ERROR = 'error';
+	private static $typeList = array(self::SUCCESS, self::WARNING, self::ERROR);
+
+	public function __construct($message, $type = SUCCESS, $page = ''){
+		if( !in_array($type, self::$typeList) ){
+			$type = self::SUCCESS;
+		}
+		$this->message = $message;
+		$this->type = $type;
+		$this->page = $page;
+	}
+
+	public function add($uid = 0){
+		if( $uid == 0 ){
+			$data['message'] = $this->message;
+			$data['type'] = $this->type;
+			$data['page'] = $this->page;
+			Session::GetCurrent()->push('ucms_notifications', $data);
+		}
+
+	}
+
+	public static function AddType($name){
+		if( !in_array($name, $typeList) ){
+			$typeList[] = $name;
+			return true;
+		}
+		return false;
+	}
+
+	public static function GetPending($name){
+		// ?
+	}
+
+	public static function RemovePending($name){
+
+	} 
+
+	public static function ShowPending(){
+		if( Session::GetCurrent()->have('ucms_notifications') ){
+			$notifications = Session::GetCurrent()->get('ucms_notifications');
+			if( !is_array($notifications) ) return;
+			// var_dump($notifications);
+			foreach ($notifications as $notification) {
+				/**
+				* @todo check for correct page
+				*/
+				echo '<div class="'.$notification['type'].'">'.$notification['message'].'</div>';
+			}
+			Session::GetCurrent()->delete('ucms_notifications');
+		}
+	}
+
+	public static function ClearPending(){
+		Session::GetCurrent()->delete('ucms_notifications');
+	}
+}
+?>
