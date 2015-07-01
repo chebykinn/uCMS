364a0abcd6170d51ee01182276a9172d0b0fa8a9
IVaN4B <ivan4b69@gmail.com>
2015-07-01 23:27:26 +0500

    Moved admin classes here, added Page and Notification classes

--- core/load.php
+++ core/load.php
@@ -10,12 +10,16 @@ require_once CORE_PATH.'database/databaseConnection.php';
 require_once CORE_PATH.'database/query.php';
 require_once CORE_PATH.'cache.php';
 require_once CORE_PATH.'session.php';
+require_once CORE_PATH.'notification.php';
 require_once CORE_PATH.'extension.php';
 require_once CORE_PATH.'theme.php';
 require_once CORE_PATH.'widget.php';
 require_once CORE_PATH.'procedural/themes.php';
 require_once CORE_PATH.'extensions.php';
-require_once CORE_PATH.'urlManager.php';
+require_once CORE_PATH.'page.php';
 require_once CORE_PATH.'settings.php';
+require_once ADMIN_PATH.'controlPanel.php';
+require_once ADMIN_PATH.'manageTable.php';
+require_once ADMIN_PATH.'managePage.php';
 require_once CORE_PATH.'ucms.php';
 ?>
