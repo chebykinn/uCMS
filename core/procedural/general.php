270d7bdbe77dd8c38de5a506383d330db4e072af
IVaN4B <ivan4b69@gmail.com>
2015-07-01 23:25:36 +0500

    Removed use of URLManager

--- core/procedural/general.php
+++ core/procedural/general.php
@@ -5,12 +5,12 @@
 
 function p($string){
 	$args = func_get_args();
-	echo call_user_func_array(array(Language::getCurrent(), 'get'), $args);
+	echo call_user_func_array(array(Language::GetCurrent(), 'get'), $args);
 }
 
 function tr($string){
 	$args = func_get_args();
-	return call_user_func_array(array(Language::getCurrent(), 'get'), $args);
+	return call_user_func_array(array(Language::GetCurrent(), 'get'), $args);
 }
 
 function get_setting($name){
@@ -22,15 +22,15 @@ function get_theme_path($name){
 }
 
 function get_current_url(){
-	return URLManager::getRaw();
+	return (string)Page::GetCurrent();
 }
 
 function get_current_action(){
-	return URLManager::getCurrentAction();
+	return Page::GetCurrent()->getAction();
 }
 
 function get_current_page(){
-	return URLManager::getCurrentPage();
+	return Page::GetCurrent()->getPageNumber();
 }
 
 function generate_hash($length = 32){
