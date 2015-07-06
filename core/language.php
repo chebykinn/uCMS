29e0894ec9fc04ab561840ec32c42ef1a02f20a8
IVaN4B <ivan4b69@gmail.com>
2015-07-07 01:33:00 +0500

    Moved 'p()' and 'tr()' functions here

--- core/language.php
+++ core/language.php
@@ -65,4 +65,14 @@ class Language{
 
 	}
 }
+
+function p($string){
+	$args = func_get_args();
+	echo call_user_func_array(array(Language::GetCurrent(), 'get'), $args);
+}
+
+function tr($string){
+	$args = func_get_args();
+	return call_user_func_array(array(Language::GetCurrent(), 'get'), $args);
+}
 ?>
