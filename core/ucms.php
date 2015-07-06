db2ad20b521c100ca07c388e0d18f382e9b7af21
IVaN4B <ivan4b69@gmail.com>
2015-07-07 01:47:29 +0500

    Moved databases connecting to 'DatabaseConnection', changed theme loading process

--- core/ucms.php
+++ core/ucms.php
@@ -2,7 +2,6 @@
 // The God Object of uCMS
 class uCMS{
 	private static $instance;
-	private $databases;
 	private $startTime = 0;
 	private $stopTime = 0;
 
@@ -14,10 +13,11 @@ class uCMS{
 	}
 
 	public function init(){
-		Debug::Init();
-		
 		$this->startLoadTimer();
+
+		Debug::Init();
 		Session::Start();
+
 		@mb_internal_encoding("UTF-8");
 		register_shutdown_function(array($this, 'shutdown'));
 		
@@ -28,51 +28,7 @@ class uCMS{
 		if( version_compare(phpversion(), UCMS_MIN_PHP_VERSION, '<') ){
 			Debug::Log(tr("Obsolete PHP"), UC_LOG_CRITICAL);
 		}
-
-		if( empty($GLOBALS['databases']) || !is_array($GLOBALS['databases']) ){
-			/**
-			* @todo install
-			*/
-			Debug::Log(tr("install, no config"), UC_LOG_CRITICAL);
-		}
-		foreach ($GLOBALS['databases'] as $dbName => $dbData) {
-			try{
-				$fields = array('server', 'user', 'password', 'name', 'port', 'prefix');
-				foreach ($fields as $field) {
-					if( !isset($dbData[$field]) ){
-						/**
-						* @todo install
-						*/
-						Debug::Log(tr("install, wrong config"), UC_LOG_CRITICAL);
-					}
-				}
-				$database = new DatabaseConnection(
-					$dbData["server"], 
-					$dbData["user"], 
-					$dbData["password"], 
-					$dbData["name"], 
-					$dbData["port"], 
-					$dbData["prefix"],
-					$dbName
-				);
-				
-				/**
-				* @todo check mysql version
-				*/
-				$this->databases[$dbName] = $database;
-			}catch(Exception $e){
-				if( $e->getCode() == 1045 || $e->getCode() == 1049 ){
-					/**
-					* @todo install
-					*/
-					Debug::Log(tr("install, wrong config"), UC_LOG_CRITICAL);
-				}else{
-					uCMS::ExceptionHandler($e);
-				}
-			}
-		}
-		unset($GLOBALS['databases']); // We don't want to have global variables, so we delete this
-
+		DatabaseConnection::Init();
 		Cache::Init();
 		Session::GetCurrent()->load();
 		Settings::Load();
@@ -92,7 +48,7 @@ class uCMS{
 	public function runSite(){
 		//parse url, get page and get extension responsible for current page
 		$templateData = false;
-
+		$siteTitle = Settings::Get('site_title');
 		if( empty($siteTitle) ) $siteTitle = tr("Untitled");
 		if( Page::GetCurrent()->getAction() != ADMIN_ACTION ){
 			$themeName = Settings::Get('theme');
@@ -100,9 +56,9 @@ class uCMS{
 		}else{
 		 	// load control panel
 			ControlPanel::Init();
+			$siteTitle = ControlPanel::TITLE;
 			$themeName = ADMIN_THEME;
 		}
-		$templateData = Extensions::LoadOnAction( Page::GetCurrent()->getAction() );
 
 		try{
 			Theme::SetCurrent($themeName);
@@ -112,8 +68,17 @@ class uCMS{
 			p("[@s]: ".$e->getMessage(), $themeName);
 		}
 
-		Theme::GetCurrent()->setTitle($templateData['title']);
-		Theme::GetCurrent()->setAction($templateData['template']);
+		$isUsed = Extensions::LoadOnAction( Page::GetCurrent()->getAction() );
+		// Debug::PrintVar($isUsed);
+		// exit;
+		if( !$isUsed ){
+			Theme::LoadErrorPage(404);
+		}else{
+			if( !Theme::GetCurrent()->IsTitleSet() ){
+				Theme::GetCurrent()->setTitle($siteTitle);
+			}
+		}
+		
 		Theme::GetCurrent()->load();
 	}
 
@@ -147,20 +112,5 @@ class uCMS{
 		return number_format( ($stopTime - $this->startTime), 3 );
 	}
 
-	public function reloadTheme($newTheme){
-		try{
-			Theme::SetCurrent($newTheme);
-		}catch(InvalidArgumentException $e){
-			p("[@s]: ".$e->getMessage(), $newTheme);
-		}catch(RuntimeException $e){
-			p("[@s]: ".$e->getMessage(), $newTheme);
-		}
-	}
-
-	public function getDatabase($name){
-		if( isset($this->databases[$name]) ){
-			return $this->databases[$name];
-		}
-	}
 }
 ?>
