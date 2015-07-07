3b4348be343a999d274288d8d04fc16b6787faab
IVaN4B <ivan4b69@gmail.com>
2015-07-07 11:29:26 +0500

    Changed loading process, started implementing regions and blocks

--- core/theme.php
+++ core/theme.php
@@ -2,7 +2,9 @@
 class Theme extends Extension{
 	private static $instance;
 	private $title;
-	private $action;
+	private $themeTemplate = GENERAL_TEMPLATE;
+	private $regions = array();
+	private $errorCode = 0;
 
 	public static function SetCurrent($themeName){
 		self::$instance = new self($themeName);
@@ -18,6 +20,55 @@ class Theme extends Extension{
 			return false;
 		}
 		return self::$instance;
+	}
+
+	public static function ReloadTheme($newTheme){
+		try{
+			Theme::SetCurrent($newTheme);
+		}catch(InvalidArgumentException $e){
+			p("[@s]: ".$e->getMessage(), $newTheme);
+		}catch(RuntimeException $e){
+			p("[@s]: ".$e->getMessage(), $newTheme);
+		}
+	}
+
+	public static function LoadErrorPage($errorCode){
+		Debug::Log(tr("Error @s at action: @s", $errorCode,
+						Page::GetCurrent()->getAction()), UC_LOG_WARNING);
+		$theme = Settings::get('theme');
+		if( empty($theme) ) $theme = DEFAULT_THEME;
+		if( !self::IsLoaded() || $theme != self::GetCurrent()->getName() ) {
+			self::ReloadTheme($theme);
+		}
+		self::GetCurrent()->setErrorCode($errorCode);
+		self::GetCurrent()->setTitle(tr("404 Not Found"));
+		self::GetCurrent()->setThemeTemplate(ERROR_TEMPLATE_NAME);
+	}
+
+	public function loadInfo(){
+		$encodedInfo = @file_get_contents($this->getExtensionInfoPath());
+		$decodedInfo = json_decode($encodedInfo, true);
+		$checkRequiredFields = empty($decodedInfo['version']) || empty($decodedInfo['coreVersion']);
+		if( $decodedInfo === NULL || $checkRequiredFields ){
+			Debug::Log(tr("Can't get theme information @s", $this->name), UC_LOG_ERROR);
+			throw new InvalidArgumentException("Can't get theme information");
+		}
+		$this->version = $decodedInfo['version'];
+		$this->coreVersion = $decodedInfo['coreVersion'];
+
+		$this->dependencies = !empty($decodedInfo['dependencies']) ? $decodedInfo['dependencies'] : "";
+		$this->info         = !empty($decodedInfo['info'])         ? $decodedInfo['info']         : "";
+		$this->regions      = !empty($decodedInfo['regions']) ? $decodedInfo['regions'] : array();
+		if( !is_array($this->regions) ) $this->regions = array($this->regions);
+	}
+
+
+	public function getErrorCode(){
+		return $this->errorCode;
+	}
+
+	public function setErrorCode($code){
+		$this->errorCode = (int) $code;
 	}
 
 	protected function getPath(){
@@ -44,11 +95,49 @@ class Theme extends Extension{
 	}
 
 	public function load(){
-		$this->includeFile(GENERAL_TEMPLATE_NAME.'.php');
+		include_once(PAGE_TEMPLATE);
+	}
+
+	public function setThemeTemplate($name){
+		if( file_exists($this->getFilePath($this->getInfo($name))) ){
+			$this->themeTemplate = $this->getInfo($name);
+		}
 	}
 
 	public function loadTemplate($name){
 		$this->includeFile($name.'.php');
+	}
+
+	public function loadStyles(){
+		/**
+		* @todo add default css'es if needed
+		*/
+		$style = $this->getInfo('style');
+		if( empty($style) ) return;
+		if( !is_array($style) ) $style = array($style);
+		foreach ($style as $css) {
+			if( file_exists($this->getFilePath($css)) ){
+				$cssHref = $this->getURLFilePath($css);
+				print '<link rel="stylesheet" type="text/css" href="'.$cssHref.'">'."\n";
+			}
+		}
+
+	}
+
+	public function loadScripts(){
+		/**
+		* @todo add default scripts'es if needed
+		*/
+		$script = $this->getInfo('script');
+		if( empty($script) ) return;
+		if( !is_array($script) ) $script = array($script);
+		foreach ($script as $file) {
+			if( file_exists($this->getFilePath($file)) ){
+				$scriptSrc = $this->getURLFilePath($file);
+				print '<script type="text/javascript" src="'.$scriptSrc.'"></script>'."\n";
+			}
+		}
+
 	}
 
 	public function loadBlock($name){
@@ -56,6 +145,10 @@ class Theme extends Extension{
 		if( file_exists($block) ){
 			$this->includeFile($this->getInfo($name));
 		}
+	}
+
+	public function IsTitleSet(){
+		return !empty($this->title);
 	}
 
 	public function setTitle($title){
@@ -66,12 +159,10 @@ class Theme extends Extension{
 		return $this->title;
 	}
 
-	public function setAction($action){
-		$this->action = $action;
-	}
-
-	public function getAction(){
-		return $this->action;
+	public function printRegionBlocks($name){
+		
+		if( in_array($name, $this->regions) ) return "";
+		Debug::PrintVar($name);
 	}
 }
 ?>
