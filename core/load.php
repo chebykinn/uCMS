7d00606bca66e134e5d68a098f3c623ab2f4c369
IVaN4B <ivan4b69@gmail.com>
2015-07-07 11:27:33 +0500

    Removed procedural functions and added 'Tools' class

--- core/load.php
+++ core/load.php
@@ -4,7 +4,6 @@ require_once ABSPATH.'core/constants.php';
 * @todo move files to more logical locations
 */
 require_once CORE_PATH.'debug.php';
-require_once CORE_PATH.'procedural/general.php';
 require_once CORE_PATH.'language.php';
 require_once CORE_PATH.'database/databaseConnection.php';
 require_once CORE_PATH.'database/query.php';
@@ -12,9 +11,9 @@ require_once CORE_PATH.'cache.php';
 require_once CORE_PATH.'session.php';
 require_once CORE_PATH.'notification.php';
 require_once CORE_PATH.'extension.php';
+require_once CORE_PATH.'tools.php';
 require_once CORE_PATH.'theme.php';
 require_once CORE_PATH.'widget.php';
-require_once CORE_PATH.'procedural/themes.php';
 require_once CORE_PATH.'extensions.php';
 require_once CORE_PATH.'page.php';
 require_once CORE_PATH.'settings.php';
