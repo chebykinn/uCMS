7bd2dcaaf3c1f6ddb225fbadec3d84f533f9122b
IVaN4B <ivan4b69@gmail.com>
2015-07-20 22:10:45 +0500

    Implemented 'AbstractExtension' class

--- core/Extensions/AbstractExtension.php
+++ core/Extensions/AbstractExtension.php
@@ -6,7 +6,85 @@
 *
 * @since 2.0
 */
-class AbstractExtension{
-	
+namespace uCMS\Core\Extensions;
+use uCMS\Core\Debug;
+use uCMS\Core\uCMS;
+abstract class AbstractExtension{
+	const INFO = 'extension.info';
+	const PATH = 'content/extensions/';
+	protected $name;
+	protected $version;
+	protected $coreVersion;
+	protected $dependencies = NULL;
+	protected $info;
+	protected $decodedInfo;
+
+	public function __construct($name){
+		$this->name = $name;
+		$this->loadInfo();
+		$this->checkCoreVersion();
+	}
+
+	protected function loadInfo(){
+
+		$encodedInfo = @file_get_contents($this->getExtensionInfoPath());
+
+		$this->decodedInfo = json_decode($encodedInfo, true);
+		$checkRequiredFields = empty($this->decodedInfo['version']) || empty($this->decodedInfo['coreVersion']);
+		if( $this->decodedInfo === NULL || $checkRequiredFields ){
+			Debug::Log(tr("Can't get extension information @s", $this->name), Debug::LOG_ERROR);
+			throw new \InvalidArgumentException("Can't get extension information");
+		}
+		$this->version = $this->decodedInfo['version'];
+		$this->coreVersion = $this->decodedInfo['coreVersion'];
+
+		$this->dependencies = !empty($this->decodedInfo['dependencies']) ? $this->decodedInfo['dependencies'] : "";
+		$this->info         = !empty($this->decodedInfo['info'])         ? $this->decodedInfo['info']         : "";
+	}
+
+	protected function includeFile($file){
+		if( file_exists($this->getFilePath($file)) ){
+			include $this->getFilePath($file);
+		}else{
+			Debug::Log(tr("Failed to open file @s", $this->getFilePath($file)), Debug::LOG_ERROR);
+		}
+	}
+
+	protected function getFilePath($file){
+		$path = self::IsDefault($this->name) ? ABSPATH.self::CORE_PATH : ABSPATH.self::PATH;
+		return $path."$this->name/$file";
+	}
+
+	protected function getExtensionInfoPath(){
+		return $this->getFilePath(self::INFO);
+	}
+
+	final protected function checkCoreVersion(){
+		if( version_compare(uCMS::CORE_VERSION, $this->coreVersion, '<') ){
+			Debug::Log(tr("Outdated core version @s", $this->name), Debug::LOG_ERROR);
+			throw new \RuntimeException("Outdated core version");
+		}
+	}
+
+	final public function getInfo($field){
+		if( !empty($this->info[$field]) )
+			return $this->info[$field];
+		return "";
+	}
+
+
+	final protected function getDependenciesList(){
+		return $this->dependencies;
+	}
+
+
+	final public function getName(){
+		return $this->name;
+	}
+
+	final public function getVersion(){
+		return $this->version;
+	}
+
 }
 ?>
