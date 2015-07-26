dcd8a53db2cc7a2b3cd1e601cc49a11b92f33ce5
IVaN4B <ivan4b69@gmail.com>
2015-07-27 00:57:05 +0500

    Moved admin pages to 'admin' extension'

--- core/content/themes/admin/phpinfo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-ob_start();
-phpinfo(); 
-$info = ob_get_clean();
-$outinfo = array(1 => '');
-preg_match('/<body[^>]*>(.*?)<\/body>/is', $info, $outinfo);
-echo $outinfo[1]; 
-?>
--- core/content/themes/admin/tools.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<h2>'.tr('Tools').'</h2><br>';
-?>
