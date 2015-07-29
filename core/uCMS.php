006bb293efc545e48735033b0c53695821755667
IVaN4B <ivan4b69@gmail.com>
2015-07-29 11:27:12 +0500

    Moving to the next Alpha version

--- core/uCMS.php
+++ core/uCMS.php
@@ -2,7 +2,7 @@
 namespace uCMS\Core;
 
 class uCMS{
-	const CORE_VERSION = "2.0 Alpha 3";
+	const CORE_VERSION = "2.0 Alpha 4";
 	const COMPATIBILITY_VERSION = "2.x";
 	const MIN_PHP_VERSION = "5.3";
 	const MIN_MYSQL_VERSION = "5.0";
