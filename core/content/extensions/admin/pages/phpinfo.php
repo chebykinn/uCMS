153298ce9da931fac8c6425150a28069bc749cf9
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:25:36 +0500

    Cosmetic changes

--- core/content/extensions/admin/pages/phpinfo.php
+++ core/content/extensions/admin/pages/phpinfo.php
@@ -4,5 +4,7 @@ phpinfo();
 $info = ob_get_clean();
 $outinfo = array(1 => '');
 preg_match('/<body[^>]*>(.*?)<\/body>/is', $info, $outinfo);
-echo $outinfo[1]; 
+$out = str_replace('<table', '<table class="manage"', $outinfo[1]);
+$out = str_replace('<hr />', "", $out);
+echo $out; 
 ?>
