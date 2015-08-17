84216c63d5e6e890dcac697bc066fad2bbe79268
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:24:45 +0500

    Moved logs parsing to Debug class

--- core/content/extensions/admin/pages/journal.php
+++ core/content/extensions/admin/pages/journal.php
@@ -1,46 +1,29 @@
 <?php
 use uCMS\Core\Debug;
-$journalLines = @file(Debug::GetLogFile());
-if(!empty($journalLines)){
-	$journalLines = array_reverse($journalLines);
-}else{
-	$journalLines = array();
+use uCMS\Core\Admin\ManageTable;
+$table = new ManageTable();
+
+$permission = 'access control panel';
+$table->setInfo('emptyMessage', tr('There are no log messages to display'));
+
+$table->addColumn(tr('Type'), false, $permission, '10%', true);
+$table->addColumn(tr('Message'), false, $permission, 0, true);
+$table->addColumn(tr('Host'), false, $permission, '10%');
+$table->addColumn(tr('Owner'), false, $permission, '10%');
+$table->addColumn(tr('Date'), true, $permission, '15%', true);
+
+$messages = Debug::GetLogMessages();
+
+foreach ($messages as $message) {
+	$table->addRow(array(
+			$message['type'],
+			$message['text'],
+			$message['host'],
+			$message['owner'],
+			$message['date']
+		)
+	);
 }
-$headerLimit = 7;
-$dateOffset = 0;
-$hostOffset = 3;
-$typeOffset = 4;
-$ownerOffset = 5;
-$messageOffset = 6;
+
+$table->printTable();
 ?>
-<table class="manage">
-<tr>
-	<th class="always-show"><?php p("Type"); ?></th>
-	<th class="always-show"><?php p("Message"); ?></th>
-	<th class="always-show"><?php p("Host"); ?></th>
-	<th class="always-show"><?php p("Owner"); ?></th>
-	<th class="always-show"><?php p("Date"); ?></th>
-</tr>
-<?php
-if(count($journalLines) > 0){
-	foreach ($journalLines as $line) {
-		$data = explode(" ", $line, $headerLimit);
-		?>
-		<tr>
-		<td class="always-show"><?php echo preg_replace("/\[|\]/", "", $data[$typeOffset]); ?></td>
-		<td class="always-show"><?php echo htmlspecialchars($data[$messageOffset]); ?></td>
-		<td class="always-show"><?php echo substr($data[$hostOffset], 0, -1)?></td>
-		<td class="always-show"><?php echo htmlspecialchars($data[$ownerOffset]); ?></td>
-		<td class="always-show"><?php echo $data[$dateOffset].' '.$data[$dateOffset+1]; ?></td>
-		</tr>
-		<?php
-	}
-}else{
-	?>
-	<tr>
-		<td colspan="4"><?php p("There are no log messages to display"); ?></td>
-	</tr>
-	<?php
-}
-?>
-</table>
