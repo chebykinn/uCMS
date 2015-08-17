a14478a829bcb13ce7b396d9cc08db9674c86f03
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:28:08 +0500

    Started implementing required methods

--- core/content/extensions/entries/extension.php
+++ core/content/extensions/entries/extension.php
@@ -2,14 +2,21 @@
 namespace uCMS\Core\Extensions\Entries;
 use uCMS\Core\Page;
 use uCMS\Core\Extensions\Theme;
+use uCMS\Core\Block;
+use uCMS\Core\Admin\ControlPanel;
+use uCMS\Core\uCMS;
+use uCMS\Core\Settings;
+use uCMS\Core\Database\Query;
 class Entries extends \uCMS\Core\Extensions\Extension implements \uCMS\Core\Extensions\IExtension {
 
 	public function onLoad(){
-		
 	}
 	
-	public function onInstall(){
-
+	public function onInstall($stage){
+		Block::Add("entries-list");
+		Settings::Add('entries_amount', 0);
+		Settings::Add('entry_types_amount', 2);
+		Settings::Add('categories_amount', 1);
 	}
 
 	public function onUninstall(){
@@ -21,15 +28,17 @@ class Entries extends \uCMS\Core\Extensions\Extension implements \uCMS\Core\Exte
 	}
 	
 	public function onAction($action){
-		if($action == Page::INDEX_ACTION){
-			return array("template" => Page::INDEX_ACTION);
-		}else{
-			Theme::LoadErrorPage(404);
-		}
+		
 	}
 
 	public function onAdminAction($action){
-
+		$title = tr("Entries");
+		switch ($action) {
+			case 'categories':
+				$title = tr("Categories");
+			break;
+		}
+		ControlPanel::SetTitle($title);
 	}
 }
 ?>
