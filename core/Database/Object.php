04f5d1ab30dd438c6ffdbdcc885a1b5205e194a8
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:37:13 +0500

    Added abstract class for all classes that use database as source

---
+++ core/Database/Object.php
@@ -0,0 +1,32 @@
+<?php
+namespace uCMS\Core\Database;
+class Object{
+	public function __construct(){
+
+	}
+
+	public static function FromArray($data){
+
+	}
+
+	public function getID(){
+
+	}
+
+	public function getName(){
+
+	}
+
+	public static function Add($object){
+
+	}
+
+	public static function Update($object){
+
+	}
+
+	public static function Delete($object){
+
+	}
+}
+?>
