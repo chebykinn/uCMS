8504847b8cdcb594449ed1c8da8445915781e966
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:11:46 +0500

    Added stage to extension's installers

--- core/Extensions/IExtension.php
+++ core/Extensions/IExtension.php
@@ -2,7 +2,7 @@
 namespace uCMS\Core\Extensions;
 interface IExtension{
 	public function onLoad();
-	public function onInstall();
+	public function onInstall($stage);
 	public function onUninstall();
 	public function onShutdown();
 	public function onAction($action);
