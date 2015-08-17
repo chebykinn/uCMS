5344027a47961a6563a59353dee1df684415aec5
IVaN4B <ivan4b69@gmail.com>
2015-08-18 01:35:18 +0500

    Added config constants and changed GetDirectory() behavior

--- core/uCMS.php
+++ core/uCMS.php
@@ -8,9 +8,13 @@ class uCMS{
 	const MIN_MYSQL_VERSION = "5.0";
 	const ERR_NOT_FOUND = 404;
 	const ERR_FORBIDDEN = 403;
+	const CONFIG_FILE = 'config.php';
+	const CONFIG_SAMPLE = 'config-manual.php';
 
 	public static function GetDirectory(){
-		return Settings::Get('ucms_dir');
+		$storedValue = Settings::Get('ucms_dir');
+		// TODO: more complex default value
+		return empty($storedValue) ? '/' : $storedValue;
 	}
 }
 
