52b400bce32418eade2206dee08b2032005f7ddf
IVaN4B <ivan4b69@gmail.com>
2015-09-06 01:09:31 +0300

    Added an ability for core namespace to override owner, added prepareSQL method

--- core/Tools.php
+++ core/Tools.php
@@ -3,7 +3,8 @@ namespace uCMS\Core;
 use uCMS\Core\Extensions\Extension;
 use uCMS\Core\Extensions\Theme;
 class Tools{
-
+	private static $isOwnerOverridden = false;
+	private static $owner = "core";
 	public static function GenerateHash($length = 32){
 		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
 		$code = "";
@@ -16,8 +17,16 @@ class Tools{
 
 	public static function GetCurrentOwner(){
 		$name = "core";
+		if ( self::$isOwnerOverridden ){
+
+			// If method is called in anothed core method we prevent
+			// looking for extension even if it was called from it.
+			self::$isOwnerOverridden = false;
+			return self::$owner;
+		}
 		$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-		foreach ($trace as $level) {
+		// \uCMS\Core\Debug::PrintVar($trace);
+		foreach ($trace as $index => $level) {
 			foreach ($level as $key => $value) {
 				$found = false;
 				if( $key == 'file' && is_file($value) ){
@@ -39,12 +48,24 @@ class Tools{
 				}
 			}
 		}
-		// \uCMS\Core\Debug::PrintVar($name);
 		return $name;
 	}
 
-	public static function PrepareSQL($value){
+	public static function OverrideOwner($newOwner = "core"){
+		if( strpos(__NAMESPACE__, "uCMS\\Core") !== false ){
+			self::$isOwnerOverridden = true;
+			if( Extension::IsExists($newOwner) || Theme::IsExists($newOwner) ){
+				self::$owner = $newOwner;
+			}
+		}
+	}
 
+	public static function PrepareSQL($value){
+		if($value == "") return false;
+		$value = implode("", explode( "\\", $value));
+		$value = stripslashes($value);
+		$value = addcslashes($value, '%');
+		return $value;
 	}
 
 	public static function PrepareXSS($value){
