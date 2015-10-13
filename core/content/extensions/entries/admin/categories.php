a0dc509a2fefbe2cac510a6a7d16029eb98d107b
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:25:41 +0300

    Added categories listing using ORM

--- core/content/extensions/entries/admin/categories.php
+++ core/content/extensions/entries/admin/categories.php
@@ -1,1 +1,31 @@
-categories
+<?php
+use uCMS\Core\Admin\ManagePage;
+use uCMS\Core\Admin\ManageTable;
+use uCMS\Core\Settings;
+use uCMS\Core\Tools;
+use uCMS\Core\Extensions\Entries\Term;
+$page = new ManagePage();
+$table = new ManageTable();
+$page->doActions();
+$table->addSelectColumn('manage entries');
+$table->addColumn(tr('Name'), true, 'manage entries', 0, true);
+$table->addColumn(tr('Type'), true, 'manage entries', 0, true);
+$table->addColumn(tr('Created'), true, 'manage entries', 0, true);
+$limit = Settings::Get('per_page');
+$categories = (new Term())->find(array('limit' => $limit));
+foreach ($categories as $category) {
+	$table->setInfo('idKey', $category->tid);
+	$table->addRow(
+		array(
+			tr($category->name).'<br>'.tr($category->description).'<br><div class="manage-actions">'.
+				$table->manageButtons(array(
+				"Edit" => 'edit',
+				"Delete" => 'delete'
+				)).'</div>',
+			tr($category->type),
+			Tools::FormatTime($category->created)
+		)
+	);
+}
+$table->printTable();
+?>
