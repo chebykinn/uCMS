565b97a070ef326441fa1ddf4dcef89f982ea4f5
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:27:07 +0300

    Updated entries list block to use ORM API

--- core/content/extensions/entries/entries-list.load.php
+++ core/content/extensions/entries/entries-list.load.php
@@ -5,42 +5,38 @@ use uCMS\Core\Debug;
 use uCMS\Core\uCMS;
 use uCMS\Core\Database\Query;
 use uCMS\Core\Extensions\Theme;
+use uCMS\Core\Extensions\Extension;
 use uCMS\Core\Extensions\Entries\Entry;
+use uCMS\Core\Extensions\Entries\EntryType;
 use uCMS\Core\Extensions\Users\User;
 // get entry types aliases and select $action type
 // if action is not home page, then entries will have url prefix
-$type = $prefix = Page::GetCurrent()->getAction();
-if( $prefix === Page::INDEX_ACTION ){
-	$prefix = "";
-}
-$entriesAmount = Settings::Get('entries_amount');
+$prefix = Page::GetCurrent()->getAction();
+$entriesAmount = (int)Settings::Get('entries_amount');
 $commentsEnabled = false;
 $commentsCount = 0;
 $comments = array();
 $entries = array();
+$isEntryPage = false;
+if( $prefix === Page::INDEX_ACTION ){
+	$prefix = "";
+}
+// TODO: consider exclude this block from these actions
+if ( empty($prefix) || !in_array($prefix, Extension::GetUsedActions()) ){
+	if( empty($prefix) ){
+		$prefix = Settings::Get('list_entry_type');
+	}
 
-$query = new Query("{entry_types}");
-$types = $query->select("*")->where()->condition('alias', '=', $type)->execute();
-if( empty($types) ){
-	$alias = mb_substr(Page::GetCurrent()->getURL(), 1);
-	$entryQuery = new Query("{entries}");
-	$entries = $entryQuery->select("*")->where()->condition("alias", "=", $alias)->execute();
-	if( empty($entries) ){
-		Theme::LoadErrorPage(uCMS::ERR_NOT_FOUND);
+	$type = (new EntryType())->find($prefix);
+	if ( $type != NULL ){
+		$entries = $type->entries;
+	}else{
+		// Try to get entry page
+		$entries = (new Entry())->find(array('alias' => $prefix));
+		if ( empty($entries) ){
+			Theme::LoadErrorPage(uCMS::ERR_NOT_FOUND);
+		}
+		$isEntryPage = true;
 	}
-	$isEntryPage = true;
-}else{
-	foreach ($types as $type) {
-		$names[] = $type['type'];
-	}
-	$list = "?".str_repeat(",?", count($names)-1);
-	// TODO: pagination
-	$entryQuery = new Query("SELECT * FROM {entries} WHERE type IN ($list)", $names);
-	$entries = $entryQuery->execute();
-	$isEntryPage = false;
-}
-foreach ($entries as &$entry) {
-	$entry = Entry::FromArray($entry);
-}
-unset($entry); // WTF?
+} 
 ?>
