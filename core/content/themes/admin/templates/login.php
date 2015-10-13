983c716cc3ed11a02b90db3a140b7f4518f0b1a4
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:24:26 +0300

    Added login page

--- core/content/themes/admin/templates/login.php
+++ core/content/themes/admin/templates/login.php
@@ -1,4 +1,10 @@
-<div id="login-form">
-	login form
+<div id="block">
+<div class="inner">
+<?php
+	if( $this->pageTitle() ){
+		echo "<h2>".$this->pageTitle()."</h2>";
+	}
+	$loginForm->render();
+?>
 </div>
 </div>
