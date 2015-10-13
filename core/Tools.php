662c61f637e623a110801bd7128e068a1827f846
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:14:59 +0300

    Added time formatting method, which uses timezone settings

--- core/Tools.php
+++ core/Tools.php
@@ -2,11 +2,12 @@
 namespace uCMS\Core;
 use uCMS\Core\Extensions\Extension;
 use uCMS\Core\Extensions\Theme;
+use uCMS\Core\Extensions\Users\User;
 class Tools{
 	private static $isOwnerOverridden = false;
 	private static $owner = "core";
 	public static function GenerateHash($length = 32){
-		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789-";
 		$code = "";
 		$clen = strlen($chars) - 1;  
 		while (strlen($code) < $length) {
@@ -30,13 +31,17 @@ class Tools{
 			foreach ($level as $key => $value) {
 				$found = false;
 				if( $key == 'file' && is_file($value) ){
-					if( strpos($value, Extension::PATH) ){
+					if( strpos($value, Extension::PATH) !== false ){
 						$path = explode(Extension::PATH, dirname($value));
 					}else{
 						$path = explode(Theme::PATH, dirname($value));
 					}
 					if( is_array($path) && isset($path[1]) ){
 						$name = $path[1];
+						if( strpos($name, "/") !== false){
+							$name = explode("/", $name);
+							$name = $name[0];
+						}
 						if( Extension::IsLoaded($name) ){ 
 							$found = true;
 							break;
@@ -48,6 +53,7 @@ class Tools{
 				}
 			}
 		}
+		// TODO: some undefined behavior when extension and theme with the same name
 		return $name;
 	}
 
@@ -70,6 +76,22 @@ class Tools{
 
 	public static function PrepareXSS($value){
 
+	}
+
+	public static function FormatTime($time, $format = ""){
+		if( empty($format) ){
+			$format = Settings::Get('datetime_format');
+		}
+		// If user have his own timezone we will use it.
+		$timezone = User::Current()->getTimezone();
+		if( empty($timezone) ){
+			$timezone = Settings::Get('ucms_timezone');
+		}
+		$datetime = new \DateTime("@$time");
+		// DateTime ignores $timezone parameter when created from timestamp, so
+		// we have to set in explicitly.
+		$datetime->setTimezone(new \DateTimeZone($timezone)); 
+		return $datetime->format($format);
 	}
 
 }
