137f8fe653a6d723010720a945d4d0037e0896a6
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:26:07 +0300

    Update entries listing to use ORM API

--- core/content/extensions/entries/admin/entries.php
+++ core/content/extensions/entries/admin/entries.php
@@ -2,72 +2,39 @@
 use uCMS\Core\Admin\ManagePage;
 use uCMS\Core\Admin\ManageTable;
 use uCMS\Core\Database\Query;
+use uCMS\Core\Settings;
 use uCMS\Core\Extensions\Entries\Entry;
+use uCMS\Core\Extensions\Entries\EntryType;
 use uCMS\Core\Admin\ControlPanel;
 $page = new ManagePage();
 $table = new ManageTable();
 $page->doActions();
-$query = new Query("SELECT 
-	{entries}.eid,
-	{entries}.uid AS author,
-	{entries}.type,
-	{entries}.status,
-	{entries}.comments,
-	{entries}.title,
-	{entries}.alias,
-	{entries}.content,
-	{entries}.language,
-	{entries}.created,
-	{entries}.changed,
-
-	{entry_access}.uid AS accessUid,
-	{entry_access}.gid AS accessGid,
-	{entry_access}.allow AS accessAllow,
-
-	{entries}.type AS 'EntryType:type',
-	{entry_types}.name AS 'EntryType:name',
-	{entry_types}.comments AS 'EntryType:comments',
-	{entry_types}.uid AS 'EntryType:uid',
-	{entry_types}.gid AS 'EntryType:gid',
-	{entry_types}.permission AS 'EntryType:permission',
-	{entry_types}.terms AS 'EntryType:terms',
-
-	{term_taxonomy}.tid AS 'Term:tid',
-	{terms}.name AS 'Term:name',
-	{terms}.type AS 'Term:type',
-
-	{entries}.uid AS 'User:uid',
-	{users}.name AS 'User:name'
-
-	                 FROM {entries}
-	                 LEFT JOIN {users} USING(uid)
-                     LEFT JOIN {entry_access} USING(eid)
-                     LEFT JOIN {entry_types} USING(type)
-                     LEFT JOIN {term_taxonomy} USING(eid)
-                     LEFT JOIN {terms} ON {term_taxonomy}.tid = {terms}.tid");
-$entries = $query->execute();
-$entry = Entry::FromArray($entries[0]);
 $table->addSelectColumn('manage entries');
 $table->addColumn(tr('Title'), true, 'manage entries', '20%', true);
-$table->addColumn(tr('Type'), true, 'manage entries', '20%', true);
-$table->addColumn(tr('Author'), true, 'manage entries', '20%', true);
-$table->addColumn(tr('Terms'), true, 'manage entries', '20%');
-$table->addColumn(tr('Comments'), true, 'manage entries', '20%');
-$table->addColumn(tr('Created'), true, 'manage entries', true);
-\uCMS\Core\Debug::PrintVar($entry);
+$table->addColumn(tr('Type'), true, 'manage entries', true);
+$table->addColumn(tr('Author'), true, 'manage entries', true);
+$table->addColumn(tr('Terms'), true, 'manage entries');
+$table->addColumn(tr('Comments'), true, 'manage entries');
+$table->addColumn(tr('Created'), true, 'manage entries', '15%');
+
+$sort['changed'] = 'desc';
+$sort['eid'] = 'desc';
+$limit = Settings::Get("entries_per_page");
+//array('where' => array('column' => 'type', 'operator' => '=', 'value' => 'article')
+$entries = (new Entry())->find(array('limit' => $limit, 'orders' => array('created' => 'desc')));
+
 foreach ($entries as $entry) {
-	$entry = Entry::FromArray($entry);
-	$table->setInfo("idKey", $entry->getID());
-	$table->setInfo('status', $entry->getStatus());
+	$table->setInfo("idKey", $entry->eid);
+	$table->setInfo('status', $entry->status);
 	$table->addRow( array(
-		$entry->getTitle().'<br><div class="manage-actions">'.
+		'<a href="'.$entry->getLink().'">'.$entry->title.'</a><br><div class="manage-actions">'.
 		$table->manageButtons(array(
 			"Publish|Draft" => 'switch-status',
 			"Edit" => 'edit',
 			"Delete" => 'delete'
 			)).'</div>',
-		$entry->getType()->getName(),
-		$entry->getAuthor()->getName(),
+		$entry->entryType->name,
+		$entry->author->name,
 		"",
 		"",
 		$entry->getDate()
