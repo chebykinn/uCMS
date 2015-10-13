557fb4feac609ac0e7a10532da58319d83f4bbed
IVaN4B <ivan4b69@gmail.com>
2015-10-13 23:24:14 +0300

    Added access denied page

--- core/content/themes/admin/templates/access_denied.php
+++ core/content/themes/admin/templates/access_denied.php
@@ -1,4 +1,13 @@
-<div id="access-denied">
+<div id="block">
+<div class="inner">
+	<?php
+	if( $this->pageTitle() ){
+		echo "<h2>".$this->pageTitle()."</h2>";
+	}
+	?>
+	<div class="error">	
 	<?php p("You don't have access to this page."); ?>
+	</div>
+	<a class="button" href="<?php echo $homePage; ?>"><?php p("Go To Home Page"); ?></a>
 </div>
 </div>
