4f33cf635b0b8ad9f462fc392d456bd3450e47f4
IVaN4B <ivan4b69@gmail.com>
2015-10-14 13:44:58 +0300

    uCMS now requires at least PHP 5.4

--- core/uCMS.php
+++ core/uCMS.php
@@ -4,7 +4,7 @@ namespace uCMS\Core;
 class uCMS{
 	const CORE_VERSION = "2.0 Alpha 4";
 	const COMPATIBILITY_VERSION = "2.x";
-	const MIN_PHP_VERSION = "5.3";
+	const MIN_PHP_VERSION = "5.4";
 	const MIN_MYSQL_VERSION = "5.0";
 	const ERR_NOT_FOUND = 404;
 	const ERR_FORBIDDEN = 403;
