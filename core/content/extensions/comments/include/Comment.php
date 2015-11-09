bc45491fb6ed010e3b1bacf3db1d09883dcdadde
IVaN4B <ivan4b69@gmail.com>
2015-11-09 18:50:32 +0300

    Started implementing Comment class

--- core/content/extensions/comments/include/Comment.php
+++ core/content/extensions/comments/include/Comment.php
@@ -1,6 +1,17 @@
 <?php
 namespace uCMS\Core\Extensions\Comments;
-class Comment{
+use uCMS\Core\ORM\Model;
+use uCMS\Core\Tools;
+class Comment extends Model{
+	public function init(){
+		$this->tableName('comments');
+		$this->primaryKey('cid');
+		$this->belongsTo('\\uCMS\\Core\\Extensions\\Entries\\Entry', array('bind' => 'entry'));
+		$this->belongsTo('\\uCMS\\Core\\Extensions\\Users\\User', array('bind' => 'author'));
+	}
 
+	public function getDate($row){	
+		return Tools::FormatTime($row->created);
+	}
 }
 ?>
