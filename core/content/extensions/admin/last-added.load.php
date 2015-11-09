c7c1fb52f9cc663960941df86d0995b3ee8a8a89
IVaN4B <ivan4b69@gmail.com>
2015-11-09 18:32:20 +0300

    Implemented Last Added listings block for admin dashboard

--- core/content/extensions/admin/last-added.load.php
+++ core/content/extensions/admin/last-added.load.php
@@ -1,1 +1,19 @@
-<?php?>
+<?php
+use uCMS\Core\Settings;
+use uCMS\Core\Extensions\Extension;
+$limit = (int)Settings::Get('last_added_limit');
+$models = array();
+$names = json_decode(Settings::Get('last_added_models'), true);
+if ( is_array($names) ){
+	foreach ($names as $title => $modelInfo) {
+		if( class_exists($modelInfo['name']) ){
+			$model = new $modelInfo['name']();
+			$modelInfo['conditions']['limit'] = $limit;
+			$modelRows = $model->find($modelInfo['conditions']);
+			$extension = Extension::Get($modelInfo['owner']);
+			$template = $extension->getFilePath($modelInfo['template']);
+			$models[$title] = array("rows" => $modelRows, "title" => $title, "template" => $template);
+		}
+	}
+}
+?>
--- core/content/extensions/admin/templates/last-added.php
+++ core/content/extensions/admin/templates/last-added.php
@@ -1,17 +1,30 @@
 <?php
-
+if( empty($models) ):
+	echo '<table class="manage"><tr><td class="always-show">';
+	p('No content selected to display.');
+	echo '</td></tr></table>';
+else:
+	foreach ($models as $title => $data) {
+		?>
+		<table class="manage last-added">
+		<tr>
+		<th class="always-show"><?php p($title); ?></th>
+		</tr>
+		<?php
+		if ( !empty($data['rows']) ){
+			foreach ($data['rows'] as $row) {
+				echo '<tr><td class="always-show">';
+				if( file_exists($data['template']) ){
+					include $data['template'];
+				}
+				echo '</td></tr>';
+			}
+		}else{
+			echo '<tr><td class="always-show">';
+			p('No elements');
+			echo '<br><br></td></tr>';
+		}
+		echo '</table>';
+	}
+endif;
 ?>
-<table class="manage last-added">
-	<tr>
-		<th class="always-show">test1</th>
-		<th class="always-show">test2</th>
-		<th class="always-show">test3</th>
-		<th class="always-show">test4</th>
-	</tr>
-	<tr>
-		<td class="always-show">test1</td>
-		<td class="always-show">test2</td>
-		<td class="always-show">test3</td>
-		<td class="always-show">test4</td>
-	</tr>
-</table>
