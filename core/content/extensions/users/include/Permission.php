fce9e6764f89f007c9107a18070a58cd140bdb94
IVaN4B <ivan4b69@gmail.com>
2015-11-09 19:04:58 +0300

    Added an ability for extensions to store data about permssion

--- core/content/extensions/users/include/Permission.php
+++ core/content/extensions/users/include/Permission.php
@@ -1,12 +1,35 @@
 <?php
 namespace uCMS\Core\Extensions\Users;
 use uCMS\Core\ORM\Model;
+use uCMS\Core\Tools;
 class Permission extends Model{
-
+	private static $list;
 	public function init(){
 		$this->primaryKey('name');
 		$this->tableName('group_permissions');
 		$this->belongsTo('\\uCMS\\Core\\Extensions\\Users\\Group');
+	}
+
+	public static function Register($name, $title, $description){
+		$owner = Tools::GetCurrentOwner();
+		self::$list[$name] = array('title' => $title, 'description' => $description, 'owner' => $owner);
+	}
+
+	public function getInfo($row){
+		if( self::IsExists($row->name) ){
+			return self::$list[$row->name];
+		}else{
+			// TODO: handle missing info
+		}
+		return array();
+	}
+
+	public static function GetPermissionsList(){
+		return self::$list;
+	}
+
+	public static function IsExists($name){
+		return isset(self::$list[$name]);
 	}
 }
 ?>
