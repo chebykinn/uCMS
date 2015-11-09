3737444335d9c41a795b8e43218927c9ea801263
IVaN4B <ivan4b69@gmail.com>
2015-11-09 19:06:04 +0300

    Added Page class links

--- core/content/extensions/users/user-card.load.php
+++ core/content/extensions/users/user-card.load.php
@@ -3,9 +3,9 @@ use uCMS\Core\Extensions\Users\User;
 use uCMS\Core\Page;
 $user = User::Current();
 $cpanelLink = Page::ControlPanel();
-$profileLink = ""; //$user->get_profileLink();
-$userlistLink = ""; //$user->get_userlistLink();			
-$logoutLink = Page::FromAction(User::LOGOUT_ACTION); //$user->getLogoutLink();
+$profileLink = Page::FromAction(User::PROFILE_ACTION, $user->name);
+$userlistLink = Page::FromAction(User::LIST_ACTION);		
+$logoutLink = Page::FromAction(User::LOGOUT_ACTION);
 $cpanelLink = Page::ControlPanel();
 $loginForm = User::GetLoginForm();
 ?>
