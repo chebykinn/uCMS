e060f9380007ebaef239d133148ff944eb9caf1c
IVaN4B <ivan4b69@gmail.com>
2015-11-09 18:37:08 +0300

    Fixed layout for mobile devices

--- core/content/extensions/admin/pages/phpinfo.php
+++ core/content/extensions/admin/pages/phpinfo.php
@@ -6,5 +6,6 @@ $outinfo = array(1 => '');
 preg_match('/<body[^>]*>(.*?)<\/body>/is', $info, $outinfo);
 $out = str_replace('<table', '<table class="manage"', $outinfo[1]);
 $out = str_replace('<hr />', "", $out);
+$out = str_replace('<td', '<td class="always-show"', $out);
 echo $out; 
 ?>
