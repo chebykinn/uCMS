3b3be3aa93084fb98b59be8196229141ceae12df
IVaN4B <ivan4b69@gmail.com>
2015-11-25 00:05:08 +0300

    Updated config sample, added hash field

--- config-manual.php
+++ config-manual.php
@@ -6,23 +6,23 @@
 * For more information visit: {@link http://ucms.ivan4b.ru/manuals/config_file}
 * @package uCMS
 */
-$GLOBALS['databases'] = array( 
-	"default" => array( // Default Database keeps uCMS data
-			  "name"     => "%name%",  // Database name
-			  "server"   => "%server%",  // Database server
-			  "port"     => "%port%", // Database server port
-			  "user"     => "%user%",  // Login
-			  "password" => "%password%", // Password
-			  "prefix"   => "%prefix%" // Table prefix, if you want to install multiple uCMS sites, set your own one
-			)
-);
+$GLOBALS['databases'] = [ 
+	"default" => [ // Default Database keeps uCMS data
+		"name"     => "%name%",  // Database name
+		"server"   => "%server%",  // Database server
+		"port"     => "%port%", // Database server port
+		"user"     => "%user%",  // Login
+		"password" => "%password%", // Password
+		"prefix"   => "%prefix%" // Table prefix, if you want to install multiple uCMS sites, set your own one
+	]
+];
 
 if(!defined("ABSPATH"))
-	define("ABSPATH", getcwd()."/"); // Absolute path to uCMS files
+	define("ABSPATH", dirname(__FILE__)."/"); // Absolute path to uCMS files
 
 define("UCMS_DEBUG", false); // Change it to true to enter debug mode. Recommended for developers.
 
-define("UCMS_HASH_SALT", "XtDrNzhThR9iJLMDDHiBh8jYajNA5flv");
+define("UCMS_HASH_SALT", "%salt%");
 /* Loading necessary stuff */
 require_once ABSPATH."core/autoload.php";
 ?>
