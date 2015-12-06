af53f3b10a73deae2829ba8f889807a80acba2b6
IVaN4B <ivan4b69@gmail.com>
2015-12-07 00:45:47 +0300

    Added constants for default types

--- core/content/extensions/entries/include/EntryType.php
+++ core/content/extensions/entries/include/EntryType.php
@@ -5,6 +5,8 @@ use uCMS\Core\ORM\Model;
 use uCMS\Core\Settings;
 class EntryType extends Model{
 	const DEFAULT_AMOUNT = 2;
+	const ARTICLE = 'article';
+	const PAGE = 'page';
 	public function init(){
 		$limit = (int) Settings::Get('entries_per_page');
 		$this->primaryKey('type');
