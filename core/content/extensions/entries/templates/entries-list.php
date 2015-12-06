dfb65ad6b500df5de62ec899d78f7716102386bb
IVaN4B <ivan4b69@gmail.com>
2015-12-07 00:46:07 +0300

    Added edit link

--- core/content/extensions/entries/templates/entries-list.php
+++ core/content/extensions/entries/templates/entries-list.php
@@ -7,7 +7,15 @@ if( $entriesAmount > 0 ){
 		?>
 		<div class="entry">
 			<p class="title">
-				<a href="<?php echo $entry->getLink(); ?>" title="<?php echo $entry->title; ?>"><?php echo $entry->title; ?></a>
+				<?php
+					if( $isEntryPage ){
+						echo $entry->title;
+					}else{
+						?>
+						<a href="<?php echo $entry->getLink(); ?>" title="<?php echo $entry->title; ?>"><?php echo $entry->title; ?></a>
+						<?php
+					}
+				?>
 			</p>
 			<p class="content">
 				<?php echo $entry->getContent($isShort); ?>
@@ -15,6 +23,12 @@ if( $entriesAmount > 0 ){
 			<p class="info">
 				<?php $delimeter = '<span class="delimeter"> | </span>'; ?>
 				<?php echo tr('Date:').' '.$entry->getDate().$delimeter.tr('Tags:').' none'; ?>
+				<?php
+					if( $entry->uid === $currentUser->uid || $currentUser->can('manage entries') ){
+						$editLink = $entry->getEditLink();
+						echo '<a href="'.$editLink.'" title="'.tr('Edit this entry').'">'.tr('Edit').'</a>';
+					}
+				?>
 			</p>
 		</div>
 		<?php
