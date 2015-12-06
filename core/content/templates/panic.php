a8d04f78b32d60d6741ab4285830c44d49c33c73
IVaN4B <ivan4b69@gmail.com>
2015-12-07 00:55:55 +0300

    Added template for displaying fatal error messages

---
+++ core/content/templates/panic.php
@@ -0,0 +1,17 @@
+<?php
+use uCMS\Core\Loader;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php p('μCMS Error'); ?></title>
+</head>
+<body>
+	<?php
+		print '<h2>'.Loader::GetInstance()->getErrorMessage().'</h2>';
+	?>
+</body>
+</html>
+</body>
+</html>
