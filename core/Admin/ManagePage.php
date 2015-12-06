6cc24b3d1c56668236d193f0990a0a7eb1411aef
IVaN4B <ivan4b69@gmail.com>
2015-12-07 00:26:48 +0300

    Updated ManagePage class

--- core/Admin/ManagePage.php
+++ core/Admin/ManagePage.php
@@ -5,20 +5,61 @@ use uCMS\Core\Extensions\Users\User;
 use uCMS\Core\Extensions\Extension;
 class ManagePage{
 	private $adminAction;
-	private $actions = array();
+	private $actions = [];
+	private $sections = [];
+	private $currentSection = '';
 
 	public function __construct(){
 		$this->adminAction = ControlPanel::GetBaseAction();
+		$this->currentSection = Page::GetCurrent()->getKeyValue($this->adminAction);
+	}
+
+	public function addSection($name, $permission, $title){
+		if( !isset($this->sections[$name]) ){
+			$this->sections[$name] = ['name' => $name, 'permission' => $permission, 'title' => $title];
+		}
+	}
+
+	public function inSection($name){
+		if( !isset($this->sections[$name]) ) return false;
+		return ($this->currentSection === $name);
+	}
+
+	public function checkSection(){
+		$name = $this->currentSection;
+		$exitPage = Page::ControlPanel($this->adminAction);
+		if( empty($name) ) return true;
+		if( !isset($this->sections[$name]) ){
+			$exitPage->go();
+		}
+		$user = User::Current();
+		if( !$user->can($this->sections[$name]['permission']) ){
+			$exitPage->go();
+		}
+	}
+
+	public function getSection(){
+		return $this->currentSection;
+	}
+
+	public function getSectionPage($section = ""){
+		return Page::ControlPanel($this->adminAction.(!empty($section) ? '/'.$section : ''));
+	}
+
+	public function printSectionTitle(){
+		if( empty($this->currentSection) || !isset($this->sections[$this->currentSection]) ) return false;
+		print '<div class="section-title"><h3>'.$this->sections[$this->currentSection]['title'].'</h3></div>';
 	}
 
 
-	public function addAction($name, $permission, $callback, $args = array()){
-		$this->actions[$name] = array('callback' => $callback, 'args' => $args, 'permission' => $permission);
+	public function addAction($name, $permission, $callback, $args = []){
+		$this->actions[$name] = ['callback' => $callback, 'args' => $args, 'permission' => $permission];
 	}
 
 	public function doActions(){
 		$name = Page::GetCurrent()->getKeyValue($this->adminAction);
 		$exitPage = Page::ControlPanel($this->adminAction);
+
 		if( !empty($this->actions[$name]) ){
 			$key = htmlspecialchars(Page::GetCurrent()->getKeyValue($name));
 			$user = User::Current();
@@ -26,10 +67,15 @@ class ManagePage{
 				$exitPage->go();
 				return false;
 			}
-			$notification = (int)call_user_func_array($this->actions[$name]['callback'], array_merge(array($key), $this->actions[$name]['args']));
+			call_user_func_array($this->actions[$name]['callback'], array_merge([$key], $this->actions[$name]['args']));
 			
 			$exitPage->go();
 		}
+	}
+
+	public function addButton($title, Page $link, $class = ""){
+		if( empty($class) ) $class = "button";
+		print '<div class="section-button"><a class="'.$class.'" href="'.$link.'" title="'.$title.'">'.$title.'</a></div>';
 	}
 
 	public function editForm($data){
