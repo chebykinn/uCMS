cbe57d14f9cf357d133c25b7f6fc7df28522320a
IVaN4B <ivan4b69@gmail.com>
2015-12-18 18:34:13 +0300

    Rewrote Block class to implement ORM

--- core/Block.php
+++ core/Block.php
@@ -5,105 +5,62 @@ use uCMS\Core\Extensions\ExtensionHandler;
 use uCMS\Core\Extensions\Theme;
 use uCMS\Core\Extensions\ThemeHandler;
 use uCMS\Core\Admin\ControlPanel;
-class Block{
+use uCMS\Core\ORM\Model;
+class Block extends Model{
 	const SHOW_EXCEPT = 0;
 	const SHOW_LISTED = 1;
 	const SHOW_MANUAL = 2;
-	private $bid;
-	private $name;
-	private $owner;
-	private $status;
-	private $theme;
-	private $region;
-	private $cache;
-	private $renderedHTML;
-	private $visibility; // 0 - show except listed actions, 1 - show at listed actions, 2 - own code to determine pages
-	private $actions;
+
+	const CACHE_NONE = 0;
+	const CACHE_USER = 1;
+	const CACHE_GROUP = 2;
+	const CACHE_ACTION = 3;
+	const CACHE_GLOBAL = 4;
+
+	const DISABLED = 0;
+	const ENABLED = 1;
 	private static $list;
 
-	public static function Init(){
-		//load blocks data
-		// status will be zero if theme or region are not set
+	public function init(){
+		$this->tableName('blocks');
+		$this->primaryKey('bid');
+	}
+
+	public static function Load(){
+		// Load blocks data for current action and theme
 		$action = Page::GetCurrent()->getAction();
 		if( $action === ControlPanel::ACTION ){
 			$action .= '/'.ControlPanel::GetAction();
 		}
-		$query = new Query("SELECT * FROM {blocks} 
-			WHERE status = 1 AND ((visibility = 1 AND actions LIKE :action) 
-				OR (visibility = 0 AND actions NOT LIKE :action) OR visibility = 2) ORDER BY position ASC", array(":action" => "%$action%"));
-		/*
-	((visibility = 0 AND (actions = '' OR actions NOT LIKE '%:action%')) 
-			OR (visibility = 1 AND (actions <> '' OR actions LIKE '%:action%'))
-			OR visibility = 2)
-		*/
-		// TODO: actions selection
-		$blocksData = $query->execute();
-		// \uCMS\Core\Debug::PrintVar($blocksData);
-		foreach ($blocksData as $data) {
-			# code...
-			$block = self::FromArray($data);
-		// \uCMS\Core\Debug::PrintVar($block);
+
+		$theme = Theme::GetCurrent()->getName();
+		$blocks = (new Block())->find([
+			'where' => [
+				['status', '=', 1], ['theme', '=', $theme], ['visibility', '=', 1], ['actions', 'LIKE', "%$action%", 'or'],
+				['status', '=', 1], ['theme', '=', $theme], ['visibility', '=', 0], ['actions', 'NOT LIKE', "%$action%", 'or'],
+				['status', '=', 1], ['theme', '=', $theme], ['visibility', '=', 2, 'or']
+			],
+			'orders' => ['position' => 'ASC']
+		]);
+		foreach ($blocks as $block) {
 			self::$list[$block->theme][$block->region][$block->name] = $block;
+
 		}
-	}
-
-	public static function FromArray($data){
-		if( is_array($data) ){
-			$block = new self();
-			$fields = array_keys(get_object_vars($block));
-			foreach ($data as $key => $value) {
-				if( in_array($key, $fields) ){
-					$block->$key = $value;
-				}
-			}
-			return $block;
-		}
-	}
-
-	public static function Shutdown(){
-
 	}
 	
-	public function _construct($name = ""){
-		if( !empty($name) ){
-			$query = new Query("{blocks}");
-			$data = $query->select("*")->where()->condition('name', '=', $name)->execute();
-			if( !empty($data) ){
-				$block = $data[0];
-				$this->bid = $block['bid'];
-				$this->name = $block['name'];
-				$this->owner = $block['owner'];
-				$this->status = $block['status'];
-				$this->theme = $block['theme'];
-				$this->region = $block['region'];
-				$this->cache = $block['cache'];
-				$this->visibility = $block['visibility'];
-				$this->actions = $block['actions'];
-			}
-		}
-	}
+	public function render($blockRow){
 
-	public function getName(){
-		return $this->name;
-	}
+		$ownerExtension = ExtensionHandler::Get($blockRow->owner);
 
-	public function getRegion(){
-		return $this->region;
-	}
-
-	public function render(){
-		//load data
-		//$data from cache or
-		$ownerExtension = ExtensionHandler::Get($this->owner);
-
-		$loadFile = $ownerExtension->getFilePath("$this->name.load.php");
+		$loadFile = $ownerExtension->getFilePath("$blockRow->name.load.php");
 		if( true && file_exists($loadFile) ) { // TODO: add cache check
 			include_once $loadFile;
 		}else{
 			// get from cache or do nothing
 		}
-		$template = "templates/$this->name.php";
-		$theme = new Theme($this->theme);
+		$template = "templates/$blockRow->name.php";
+		$theme = new Theme($blockRow->theme);
+		// Caution: includes can overwrite previously declared variables 
 		ob_start();
 
 		include(ABSPATH.Theme::VARIABLES_LOAD);
@@ -113,103 +70,93 @@ class Block{
 		}else if( file_exists($ownerExtension->getFilePath($template)) ){
 			include_once $ownerExtension->getFilePath($template);
 		}
-		$this->renderedHTML = ob_get_clean();
+		$blockRow->renderedHTML = ob_get_clean();
+
 		// TODO: cache this render
-		return !empty($this->renderedHTML);
-		//load template from theme or extension and render it
+		return !empty($blockRow->renderedHTML);
 	}
 
-	public function printRendered(){
+	public function printRendered($row){
 		// Print rendered block content
-		if( !empty($this->renderedHTML) ){
-			print $this->renderedHTML;
+		if( !empty($row->renderedHTML) ){
+			print $row->renderedHTML;
 		}
 	}
 
-	public static function Add($name, $region = "", $theme = "", $position = -1, $visibility = self::SHOW_EXCEPT, $actions = "", $cache = 0){
+	private function prepareData($row){
+		if( !ThemeHandler::IsExists($row->theme) ) $row->theme = "";
+		if( empty($row->theme) ) $row->theme = Settings::Get('theme');
 
-		if( !ThemeHandler::IsExists($theme) ) $theme = "";
-		if( empty($theme) ) $theme = Settings::Get('theme');
-		// TODO: region check
-		// TODO: Check if block exists
-		$status = ($theme != "" && $region != "") ? 1 : 0;
-		$owner = Tools::GetCurrentOwner();
-		$query = new Query("{blocks}");
-		$check = $query->select('name')
-		->condition('theme',  '=', $theme)
-		->condition('region', '=', $region)
-		->condition('name',   '=', $name)
-		->limit(1)->execute('count');
-		if( $check === 0 ){
-			$add = $query->insert(
-				['name', 'owner', 'status', 'theme', 'region', 'visibility', 'actions', 'cache', 'position'], 
-				[[$name, $owner, $status, $theme, $region, $visibility, $actions, $cache, $position]], true
-			)->execute();
+		if( $row->status == self::ENABLED ){
+			$row->status = ($row->region != "") ? self::ENABLED : self::DISABLED;
+		}
 
+		$row->owner = Tools::GetCurrentOwner();
+
+		if( $row->visibility < self::SHOW_EXCEPT ){
+			$row->visibility = self::SHOW_EXCEPT;
+		}
+
+		if( $row->visibility > self::SHOW_MANUAL ){
+			$row->visibility = self::SHOW_MANUAL;
+		}
+
+		if( $row->cache < self::CACHE_NONE ){
+			$row->cache = self::CACHE_NONE;
+		}
+
+		if( $row->cache > self::CACHE_GLOBAL ){
+			$row->cache = self::CACHE_GLOBAL;
+		}
+
+		if( $row->position < 0 ){
+			$row->position = 0;
+		}
+	}
+
+	public function create($row){
+		$this->prepareData($row);
+		$duplicate = (new Block())->count([
+			'theme'  => $row->theme,
+			'region' => $row->region,
+			'name'   => $row->name
+		]);
+
+		if( !empty($duplicate) ) return false;
+
+		$result = parent::create($row);
+		if( $result ){
 			Settings::Increment("blocks_amount");
-			return $add;
 		}
-		return false;
+		return $result;
 	}
 
+	public function update($row){
+		$this->prepareData($row);
 
-	// TODO: not static ?
-	public static function Update($name, $region = "", $theme = "", $position = -1, $visibility = -1, $actions = "", $cache = -1){
-		if( !ThemeHandler::IsExists($theme) ) $theme = "";
-		if( empty($theme) ) $theme = Settings::Get('theme');
-		// TODO: block is exists
-		$status = ($theme != "" && $region != "") ? 1 : 0;
-		$newData = array();
-		if( !empty($theme) ){
-			$newData['theme'] = $theme;
-		}
-		if( !empty($region) ){
-			$newData['region'] = $region;
-		}
-		if( $position > 0 ){
-			$newData['position'] = $position;
-		}
-		if( $visibility > 0 ){
-			$newData['visibility'] = $visibility;
-		}
-		// TODO: Check previous
-		$newData['actions'] = $actions;
-		if( $cache > 0 ){
-			$newData['cache'] = $cache;
-		}
-		$query = new Query("{blocks}");
-		// TODO: Check duplicate
-		$update = $query->update($newData)->where()->condition("name", "=", $name)->_and()->condition('theme', '=', $theme)->execute();
-		return $update;
+		$duplicate = (new Block())->count([
+			'theme'  => $row->theme,
+			'region' => $row->region,
+			'name'   => $row->name
+		]);
+
+		if( !empty($duplicate) ) return false;
+
+		$result = parent::update($row);
+		return $result;
 	}
 
-	public static function Delete($name){
-
-	}
-
-	public static function Get($theme, $region, $name){
-		$owner = Tools::GetCurrentOwner(); 
-		if( isset(self::$list[$theme][$region][$name])
-			&& self::$list[$theme][$region][$name]->owner === $owner ){
-			return self::$list[$theme][$region][$name];
-		}else if( !empty($name) ){
-			// select from database
-			$query = new Query("{blocks}");
-			$blockData = $query->select("*")->where()->condition("name", "=", $name)
-			 ->_and()->condition("owner", "=", $owner)
-			 ->_and()->condition("theme", "=", $theme)
-			 ->_and()->condition("region", "=", $region)
-			 ->execute();
-			if( !empty($blockData) ){
-				$block = self::FromArray($blockData);
-				return $block;
-			}
+	public function delete($row){
+		$result = parent::delete($row);
+		if( $result ){
+			Settings::Decrement("blocks_amount");
 		}
+		return $result;
 	}
 
 	public static function GetList($region = "", $theme = ""){
 		if ( !ThemeHandler::IsExists($theme) ){
-			$theme = Tools::GetCurrentOwner();
+			$theme = Theme::GetCurrent()->getName();
 		}
 		if( isset(self::$list[$theme]) ){
 			if( !empty($region) ){
@@ -221,10 +168,6 @@ class Block{
 			return self::$list[$theme];
 		}
 		return array();
-	}
-
-	public function getVisibility(){
-		return $this->visibility;
 	}
 }
 ?>
