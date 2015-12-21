9029d71f35a046e5c45d0475ffa83898c7f4f4ad
IVaN4B <ivan4b69@gmail.com>
2015-12-21 18:56:36 +0300

    Table now lists menus instead of links

--- core/content/extensions/menus/admin/menu.php
+++ core/content/extensions/menus/admin/menu.php
@@ -2,28 +2,30 @@
 use uCMS\Core\Admin\ManagePage;
 use uCMS\Core\Admin\ManageTable;
 use uCMS\Core\Extensions\Menus\MenuLink;
+use uCMS\Core\Extensions\Menus\Menu;
 use uCMS\Core\Settings;
 use uCMS\Core\Tools;
 $page = new ManagePage();
 $table = new ManageTable();
 $page->doActions();
 
-$amount = (new MenuLink())->count();
+$amount = (new Menu())->count();
 
 $table->setInfo("amount", $amount);
 $table->addSelectColumn('manage menu links');
 $table->addColumn(tr('Title'), true, 'manage menu links', 0, true);
-$table->addColumn(tr('Link'), true, 'manage menu links', 0, true);
-$table->addColumn(tr('Menu'), true, 'manage menu links', 0, true);
+$table->addColumn(tr('Links'), true, 'manage menu links', 0, true);
 
 $limit = Settings::Get('per_page');
-$links = (new MenuLink())->find(array('limit' => $limit));
-foreach ($links as $link) {
-	$table->setInfo("idKey", $link->lid);
+$menus = (new Menu())->find(array('limit' => $limit));
+foreach ($menus as $menu) {
+	$table->setInfo("idKey", $menu->lid);
+	ob_start();
+	$menu->render();
+	$links = ob_get_clean();
 	$table->addRow( array(
-			tr($link->title),
-			$link->getLink(),
-			$link->menu,
+			tr($menu->title).'<br>'.tr($menu->description),
+			$links
 		)
 	);
 }
