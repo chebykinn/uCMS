57df0f36d3765f34a1951df076659eb54e6b9338
IVaN4B <ivan4b69@gmail.com>
2015-12-21 18:58:53 +0300

    Added table for menus, moved quick action menu here

--- core/content/extensions/menus/extension.php
+++ core/content/extensions/menus/extension.php
@@ -5,6 +5,7 @@ use uCMS\Core\Extensions\Theme;
 use uCMS\Core\Extensions\Users\Permission;
 use uCMS\Core\Admin\ControlPanel;
 use uCMS\Core\Block;
+use uCMS\Core\Page;
 
 class Menus extends Extension{
 	public function onLoad(){
@@ -16,12 +17,22 @@ class Menus extends Extension{
 	}
 
 	private function addBlocks(){
-		$menu = (new Block())->clean();
-		$menu->name = "menu";
-		$menu->region = "navigation";
-		$menu->theme = Theme::DEFAULT_THEME;
-		$menu->status = Block::ENABLED;
-		$menu->create();
+		$navMenu = (new Block())->clean();
+		$navMenu->name = "navigation";
+		$navMenu->region = "navigation";
+		$navMenu->theme = Theme::DEFAULT_THEME;
+		$navMenu->status = Block::ENABLED;
+		$navMenu->create();
+
+		$quickActions = (new Block())->clean();
+		$quickActions->name = "quick-actions";
+		$quickActions->region = "dashboard-left-side";
+		$quickActions->actions = ControlPanel::ACTION.'/'.Page::INDEX_ACTION;
+		$quickActions->position = 1;
+		$quickActions->theme = ControlPanel::THEME;
+		$quickActions->status = Block::ENABLED;
+		$quickActions->visibility = Block::SHOW_LISTED;
+		$quickActions->create();
 	}
 
 	protected function checkStage(){
@@ -31,6 +42,26 @@ class Menus extends Extension{
 
 	protected function getSchemas(){
 		$schemas['menus'] = [
+			'fields' => [
+				'menu' => [
+					'type' => 'varchar',
+					'size' => 'big',
+					'not null' => true
+				],
+				'title' => [
+					'type' => 'varchar',
+					'size' => 'big',
+					'not null' => true
+				],
+				'description' => [
+					'type' => 'text',
+					'size' => 'big'
+				]
+			],
+			'primary key' => 'menu'
+		];
+
+		$schemas['menu_links'] = [
 			'fields' => [
 				'lid' => [
 					'type' => 'serial',
@@ -71,6 +102,11 @@ class Menus extends Extension{
 					'size' => 'big',
 					'not null' => true
 				],
+				'permission' => [
+					'type' => 'varchar',
+					'size' => 'big',
+					'not null' => true
+				],
 				'uid' => [
 					'type' => 'int',
 					'default' => 0
@@ -94,31 +130,48 @@ class Menus extends Extension{
 	}
 
 	protected function fillTable($table){
-		$homePageLink = (new MenuLink())->clean();
-		$homePageLink->menu = 'navigation';
-		$homePageLink->title = 'Home';
-		$homePageLink->link = '';
-		$homePageLink->status = 1;
-		$homePageLink->owner = 'core';
+		switch ($table) {
+			case 'menus':
+				$navMenu = (new Menu())->clean();
+				$dashMenu = (new Menu())->clean();
+				$navMenu->menu = "navigation";
+				$navMenu->title = "Navigation";
+				$navMenu->description = "Default main navigation menu.";
+				$navMenu->create();
+		
+				$dashMenu->menu = "quick-actions";
+				$dashMenu->title = "Quick Actions";
+				$dashMenu->description = "Menu for quick actions block, displayed at the Control Panel's dashboard.";
+				$dashMenu->create();	
+			break;
+			
+			case 'menu_links':
+				$homePageLink = (new MenuLink())->clean();
+				$homePageLink->menu = 'navigation';
+				$homePageLink->title = 'Home';
+				$homePageLink->link = '';
+				$homePageLink->status = 1;
+				$homePageLink->owner = 'core';
 
-		$dashboardAddUser = (new MenuLink())->clean();
-		$dashboardAddUser->menu = 'dashboard';
-		$dashboardAddUser->title = 'Add user';
-		$dashboardAddUser->link = 'admin/users/add';
-		$dashboardAddUser->status = 1;
-		$dashboardAddUser->owner = 'admin';
+				$dashboardAddUser = (new MenuLink())->clean();
+				$dashboardAddUser->menu = 'quick-actions';
+				$dashboardAddUser->title = 'Add user';
+				$dashboardAddUser->link = 'admin/users/add';
+				$dashboardAddUser->status = 1;
+				$dashboardAddUser->owner = 'admin';
 
-		$dashboardAddEntry = (new MenuLink())->clean();
-		$dashboardAddEntry->menu = 'dashboard';
-		$dashboardAddEntry->title = 'Add entry';
-		$dashboardAddEntry->link = 'admin/entries/add';
-		$dashboardAddEntry->status = 1;
-		$dashboardAddEntry->owner = 'admin';
+				$dashboardAddEntry = (new MenuLink())->clean();
+				$dashboardAddEntry->menu = 'quick-actions';
+				$dashboardAddEntry->title = 'Add entry';
+				$dashboardAddEntry->link = 'admin/entries/add';
+				$dashboardAddEntry->status = 1;
+				$dashboardAddEntry->owner = 'admin';
 
-		$homePageLink->create();
-		$dashboardAddUser->create();
-		$dashboardAddEntry->create();
-		return false;
+				$homePageLink->create();
+				$dashboardAddUser->create();
+				$dashboardAddEntry->create();
+			break;
+		}
 	}
 }
 ?>
