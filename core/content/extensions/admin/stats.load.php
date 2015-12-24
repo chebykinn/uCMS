82a44d7e2991251b84921f21e7fe012a3e058a95
IVaN4B <ivan4b69@gmail.com>
2015-12-24 22:56:14 +0300

    Added comments amount value

--- core/content/extensions/admin/stats.load.php
+++ core/content/extensions/admin/stats.load.php
@@ -10,23 +10,24 @@ $users = (int) Settings::Get('users_amount');
 $groups = (int) Settings::Get('groups_amount');
 $entries = (int) Settings::Get('entries_amount');
 $categories = (int) Settings::Get('categories_amount');
+$comments = (int) Settings::Get('comments_amount');
 $blocks = (int) Settings::Get('blocks_amount');
 $extensions = count(ExtensionHandler::GetList());
 $themes = count(ThemeHandler::GetList());
 $coreVersion = uCMS::CORE_VERSION;
 $siteName = Settings::Get('site_name');
-$currentTime = Tools::FormatTime(time());
+$currentTime = Tools::FormatTime();
 $currentTheme = (new Theme(Settings::Get('theme')))->getInfo('displayname');
 $domain = uCMS::GetDomain();
 $directory = uCMS::GetDirectory();
-$stats = array(
+$stats = [
 	"Users" => $users,
 	"Groups" => $groups,
 	"Categories" => $categories,
 	"Entries" => $entries,
-	"Comments" => 0,
+	"Comments" => $comments,
 	"Extensions" => $extensions,
 	"Themes" => $themes,
 	"Blocks" => $blocks
-);
+];
 ?>
