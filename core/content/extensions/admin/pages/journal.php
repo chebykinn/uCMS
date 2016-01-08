e17f8ab6b647bebf2bb891c5c02ece84a43210b7
IVaN4B <ivan4b69@gmail.com>
2016-01-09 01:11:02 +0500

    Renamed field for its new meaning, updated lines counting

--- core/content/extensions/admin/pages/journal.php
+++ core/content/extensions/admin/pages/journal.php
@@ -20,21 +20,20 @@ $messages = Debug::GetLogMessages();
 
 $permission = 'access control panel';
 $table->setInfo('emptyMessage', $this->tr('There are no log messages to display'));
-$table->setInfo("amount", count($messages));
-
+$table->setInfo("amount", Debug::GetLogLinesCount());
 $table->addColumn($this->tr('Type'), false, $permission, '10%', true);
 $table->addColumn($this->tr('Message'), false, $permission, 0, true);
 $table->addColumn($this->tr('Host'), false, $permission, '10%');
-$table->addColumn($this->tr('Owner'), false, $permission, '10%');
+$table->addColumn($this->tr('Sender'), false, $permission, '10%');
 $table->addColumn($this->tr('Date'), true, $permission, '15%', true);
 
 
 foreach ($messages as $message) {
 	$table->addRow(array(
-			$message['type'],
+			$this->tr($message['type']),
 			$message['text'],
 			$message['host'],
-			$message['owner'],
+			$message['sender'],
 			$message['date']
 		)
 	);
